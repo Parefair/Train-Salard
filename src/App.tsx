bf9a7133b21f35c9d6e893f2d2b34bb5d6fcdd77
Parefair <Kanyaluck.cc@gmail.com>
2024-06-09 22:15:37 +0700

    formatted after Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <h1>Train Salard</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
