8dd50b9510d126f43ca615a583f3a80c17f33bd3
Parefair <Kanyaluck.cc@gmail.com>
2024-06-10 19:49:33 +0700

    Version1: Set up Tailwind & first code version

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,73 @@
-import React from 'react'
 import { render, screen } from '@testing-library/react'
+import user from '@testing-library/user-event'
 import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+describe('App', () => {
+  test('renders Train Salard', () => {
+    render(<App />)
+    const topicElement = screen.getByRole('heading', {
+      name: "เทรน Sa-lard"
+    })
+    expect(topicElement).toBeInTheDocument()
+  
+    const inputElement = screen.getByRole("textbox", {
+      name: "ข้อความก่อน debut"
+    })
+    expect(inputElement).toBeInTheDocument()
+  
+    const outputElement = screen.getByRole("textbox", {
+      name: "ข้อความหลัง debut"
+    })
+    expect(outputElement).toBeInTheDocument()
+  
+    const translateButtonElement = screen.getByRole("button", {
+      name: "แปล"
+    })
+    expect(translateButtonElement).toBeInTheDocument()
+    
+    const clearButtonElement = screen.getByRole("button", {
+      name: "ล้าง"
+    })
+    expect(clearButtonElement).toBeInTheDocument()
+  })
+
+  test('render translate button', async() => {
+    user.setup()
+    render(<App />)
+    const inputElement = screen.getByRole("textbox", {
+      name: "ข้อความก่อน debut"
+    })
+
+    const outputElement = screen.getByRole("textbox", {
+      name: "ข้อความหลัง debut"
+    })
+
+    const translateButtonElement = screen.getByRole("button", {
+      name: "แปล"
+    })
+
+    await user.type(inputElement, '้ำสสน l;ylfu');
+    await user.click(translateButtonElement)
+    expect(outputElement).toHaveValue('hello สวัสดี');
+  })
+
+  test('render clear button', async() => {
+    user.setup()
+    render(<App />)
+    const inputElement = screen.getByRole("textbox", {
+      name: "ข้อความก่อน debut"
+    })
+
+    const outputElement = screen.getByRole("textbox", {
+      name: "ข้อความหลัง debut"
+    })
+
+    const translateButtonElement = screen.getByRole("button", {
+      name: "แปล"
+    })
+
+    await user.click(translateButtonElement)
+    expect(inputElement).toHaveValue('');
+    expect(outputElement).toHaveValue('');
+  })
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,tsx,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue': '#232946',
+        'purple': '#B8C1EC',
+        'pink': '#EEBBC3',
+        'white': '#FFFFFE',
+      },
+    },
+    fontFamily: {
+      'ibm-plex-sans-thai-looped': ['"IBM Plex Sans Thai Looped"', 'sans-serif']
+    },
   },
   plugins: [],
-}
 
+}
